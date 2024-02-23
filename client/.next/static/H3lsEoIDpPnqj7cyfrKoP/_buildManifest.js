080550c33a875a4e3c1875b41d390fb300b1283b
Zofia Kaska <zkask001@gold.ac.uk>
2024-02-23 14:34:28 +0000

    Removing .next folder from client, remains just in root

--- client/.next/static/H3lsEoIDpPnqj7cyfrKoP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-3967261c757b164b.js"],"/_error":["static/chunks/pages/_error-5feb345ab3932fe4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
