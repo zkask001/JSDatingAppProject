03b7847851459035040e11ccbe33f6774b2d7a16
Zofia Kaska <zkask001@gold.ac.uk>
2024-02-23 12:19:50 +0000

    Initial commit - Hello World basic app initialised

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from '../components/HelloWorld';
+const Home = () => {
+    return (
+       <div>
+            <h1>Welcome to my web app</h1>
+            <HelloWorld />
+       </div>
+     );
+};
+    
+export default Home;
