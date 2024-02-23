3171c8844bda9fd92c369f2581bf34f61410751d
Zofia Kaska <zkask001@gold.ac.uk>
2024-02-23 13:03:56 +0000

    Adding server and separating project into server and client

---
+++ client/.next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"ccf16e7e691b4eac801ff5d1c3d0d4ce\",\"previewModeSigningKey\":\"435f193c85057787aea943e02d7b4870c4e938ab9f919fd406185d28a1ae3aaa\",\"previewModeEncryptionKey\":\"787d5623f6d2d402e1cf33405f5a9435509030668cf06faa016bdb6f37ec856c\"}}"
